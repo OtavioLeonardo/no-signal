1c8d1ecdf665dbf3f9307c3a6bce17de8ffeb4de
曲嘉浩 <garrosh_qu@outlook.com>
2026-03-24 10:24:48 +0800

    日记加密

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare module '@pagefind/default-ui';
+
+interface ImportMetaEnv {
+	readonly PUBLIC_DIARY_PASSCODE_SHA256?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
